3d42dcff62480ffc6102c66fa43a0531c98a385c
DHRUB DAS <dhrub27das@gmail.com>
2025-11-07 17:13:43 +0530

    Basic changes

--- day3/modules_/ec2-instance/main.tf
+++ day3/modules_/ec2-instance/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = var.ami_value
   instance_type = var.instance_type_value
   subnet_id     = var.subnet_id_value
