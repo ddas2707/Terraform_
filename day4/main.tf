03aab8ac41a0f22ae7f9b6a3fecad974811eab15
DHRUB DAS <dhrub27das@gmail.com>
2025-11-07 17:59:14 +0530

    day4 completed

---
+++ day4/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example2" {
+  instance_type = "t2.micro"
+  ami           = "ami-02d26659fd82cf299"
+  subnet_id     = "subnet-01f082dede768aefa"
+}
