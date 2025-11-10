d903a49718362e586eaf6d891867144ff1bc8efb
DHRUB DAS <dhrub27das@gmail.com>
2025-11-10 12:14:39 +0530

    backend configured using dynamodb with locking mechanisms

--- day4/backend.tf
+++ day4/backend.tf
@@ -1,8 +1,21 @@
+# this backend configuration is implement simple backend mechanism with s3 bucket
+# terraform {
+#   backend "s3" {
+#     bucket = "dhrub-s3-bucket"
+#     key    = "day4/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
+
+
+
+# this backend configuration is implement backend mechanism with dynamodb table for locking
 terraform {
   backend "s3" {
-    bucket = "dhrub-s3-bucket"
-    key    = "day4/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "dhrub-s3-bucket"
+    key            = "day4/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
   }
 }
 
--- day4/main.tf
+++ day4/main.tf
@@ -9,3 +9,13 @@ provider "aws" {
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = "dhrub-s3-bucket"
 }
+
+resource "aws_dynamodb_table" "terraform-lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
